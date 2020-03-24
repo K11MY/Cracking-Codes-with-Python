888503e14ad11dc131486ff76bbfc574a5ca2f9e
K11MY <kimpham99@protonmail.com>
2020-03-24 15:20:45 +1100

    Testing for transposition cipher

---
+++ venv/detectEnglish.py
@@ -0,0 +1,39 @@
+UPPERLETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+LETTERS_AND_SPACES = UPPERLETTERS + UPPERLETTERS.lower() + ' \t\n'
+
+def loadDictionary():
+    dictionaryFile = open('dictionary.txt')
+    englishWords = {}
+    for word in dictionaryFile.read().split('\n'):
+        englishWords[words] = None
+    dictionaryFile.close()
+    return englishWords
+ENGLISH_WORDS = loadDictionary()
+
+def getEnglishCount(message):
+    message = message.upper()
+    message = removeNonletters(message)
+    possibleWords = message.split()
+
+    if possibleWords == []:
+        return 0.0 #No words at all so return 0.0
+
+    #Count all the matches
+    matches = 0
+    for word in possibleWords:
+        if word in ENGLISH_WORDS:
+            matches +=1
+    return float(matches) / possibleWords
+
+def removeNonLetters(message):
+    lettersOnly = []
+    for symbol in message:
+        lettersOnly.append(symbol)
+    return ''.join(lettersOnly)
+
+def isEnglish(message, wordPercentage, letterPercentage):
+    wordsMatch = getEnglishCount(message) * 100 >= wordPercentage
+    numLetters = len(removeNonLetters(message))
+    messageLettersPercentage = float(numLetters) / len(message) * 100
+    lettersMatch = messageLettersPercentage >= letterPercentage
+    return wordsMatch and lettersMatch
